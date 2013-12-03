3cd94128df94ea903866ba201f6cfc0f351f55b6
shin5ok <shin5ok@55mp.com>
2013-12-03 10:41:45 +0900

    modified to accept data from direct apnic site

--- bin/delegated-apnic-latest-register.pl
+++ bin/delegated-apnic-latest-register.pl
@@ -2,11 +2,17 @@
 use strict;
 use warnings;
 use NetAddr::IP;
+use LWP::UserAgent;
 
 use Redis;
-my $r = Redis->new;
+my $r  = Redis->new;
+my $ua = LWP::UserAgent->new;
+
+my $response = $ua->get(qq{http://ftp.apnic.net/stats/apnic/delegated-apnic-latest});
+my $content  = $response->content;
+
 # awk -F '|' 'BEGIN{log2=log(2)}{if($2=="JP" && $3=="ipv4"){print $4"/"32 - log($5) / log2}}' delegated-apnic-latest
-while (<>) {
+for (split /\n/, $content) {
   # apnic|JP|ipv4|1.1.64.0|16384|20110412|allocated
   m{^[^\|]+\|([^\|]+)\|ipv4\|\d+} or next;
   my @p = split /\|/, $_;
